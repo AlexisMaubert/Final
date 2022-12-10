56054eebd706e7d831ff2d20eaebce4ae90176c3
Alexis J Maubert <alexis.maubert@davinci.edu.ar>
2022-12-09 22:16:07 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/MovimientoController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Final.Data;
+using Final.Models;
+
+namespace Final.Controllers
+{
+    public class MovimientoController : Controller
+    {
+        private readonly MiContexto _context;
+
+        public MovimientoController(MiContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Movimiento
+        public async Task<IActionResult> Index()
+        {
+            var miContexto = _context.movimientos.Include(m => m.caja);
+            return View(await miContexto.ToListAsync());
+        }
+
+        // GET: Movimiento/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.movimientos == null)
+            {
+                return NotFound();
+            }
+
+            var movimiento = await _context.movimientos
+                .Include(m => m.caja)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (movimiento == null)
+            {
+                return NotFound();
+            }
+
+            return View(movimiento);
+        }
+
+        // GET: Movimiento/Create
+        public IActionResult Create()
+        {
+            ViewData["id_Caja"] = new SelectList(_context.cajas, "id", "id");
+            return View();
+        }
+
+        // POST: Movimiento/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,detalle,monto,fecha,id_Caja")] Movimiento movimiento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movimiento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["id_Caja"] = new SelectList(_context.cajas, "id", "id", movimiento.id_Caja);
+            return View(movimiento);
+        }
+
+        // GET: Movimiento/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.movimientos == null)
+            {
+                return NotFound();
+            }
+
+            var movimiento = await _context.movimientos.FindAsync(id);
+            if (movimiento == null)
+            {
+                return NotFound();
+            }
+            ViewData["id_Caja"] = new SelectList(_context.cajas, "id", "id", movimiento.id_Caja);
+            return View(movimiento);
+        }
+
+        // POST: Movimiento/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,detalle,monto,fecha,id_Caja")] Movimiento movimiento)
+        {
+            if (id != movimiento.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movimiento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovimientoExists(movimiento.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["id_Caja"] = new SelectList(_context.cajas, "id", "id", movimiento.id_Caja);
+            return View(movimiento);
+        }
+
+        // GET: Movimiento/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.movimientos == null)
+            {
+                return NotFound();
+            }
+
+            var movimiento = await _context.movimientos
+                .Include(m => m.caja)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (movimiento == null)
+            {
+                return NotFound();
+            }
+
+            return View(movimiento);
+        }
+
+        // POST: Movimiento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.movimientos == null)
+            {
+                return Problem("Entity set 'MiContexto.movimientos'  is null.");
+            }
+            var movimiento = await _context.movimientos.FindAsync(id);
+            if (movimiento != null)
+            {
+                _context.movimientos.Remove(movimiento);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovimientoExists(int id)
+        {
+          return _context.movimientos.Any(e => e.id == id);
+        }
+    }
+}
---
+++ Controllers/PagoController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Final.Data;
+using Final.Models;
+
+namespace Final.Controllers
+{
+    public class PagoController : Controller
+    {
+        private readonly MiContexto _context;
+
+        public PagoController(MiContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Pago
+        public async Task<IActionResult> Index()
+        {
+            var miContexto = _context.pagos.Include(p => p.usuario);
+            return View(await miContexto.ToListAsync());
+        }
+
+        // GET: Pago/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.pagos == null)
+            {
+                return NotFound();
+            }
+
+            var pago = await _context.pagos
+                .Include(p => p.usuario)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pago == null)
+            {
+                return NotFound();
+            }
+
+            return View(pago);
+        }
+
+        // GET: Pago/Create
+        public IActionResult Create()
+        {
+            ViewData["id_usuario"] = new SelectList(_context.usuarios, "id", "apellido");
+            return View();
+        }
+
+        // POST: Pago/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,id_usuario,nombre,monto,pagado,metodo")] Pago pago)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pago);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["id_usuario"] = new SelectList(_context.usuarios, "id", "apellido", pago.id_usuario);
+            return View(pago);
+        }
+
+        // GET: Pago/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.pagos == null)
+            {
+                return NotFound();
+            }
+
+            var pago = await _context.pagos.FindAsync(id);
+            if (pago == null)
+            {
+                return NotFound();
+            }
+            ViewData["id_usuario"] = new SelectList(_context.usuarios, "id", "apellido", pago.id_usuario);
+            return View(pago);
+        }
+
+        // POST: Pago/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,id_usuario,nombre,monto,pagado,metodo")] Pago pago)
+        {
+            if (id != pago.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pago);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PagoExists(pago.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["id_usuario"] = new SelectList(_context.usuarios, "id", "apellido", pago.id_usuario);
+            return View(pago);
+        }
+
+        // GET: Pago/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.pagos == null)
+            {
+                return NotFound();
+            }
+
+            var pago = await _context.pagos
+                .Include(p => p.usuario)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pago == null)
+            {
+                return NotFound();
+            }
+
+            return View(pago);
+        }
+
+        // POST: Pago/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.pagos == null)
+            {
+                return Problem("Entity set 'MiContexto.pagos'  is null.");
+            }
+            var pago = await _context.pagos.FindAsync(id);
+            if (pago != null)
+            {
+                _context.pagos.Remove(pago);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PagoExists(int id)
+        {
+          return _context.pagos.Any(e => e.id == id);
+        }
+    }
+}
